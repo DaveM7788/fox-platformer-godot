8a82b995c9c7c7ff7cd65b305971f2a494337208
DaveM7788 <david.mcdermott98@gmail.com>
2025-12-02 23:32:07 -0600

    layers for bullets
    enemies can be shot now

--- Scenes/Bullets/shooter.gd
+++ Scenes/Bullets/shooter.gd
@@ -26,7 +26,6 @@ func shoot(direction: Vector2) -> void:
 	if !_can_shoot:
 		return
 	_can_shoot = false
-	print("SHOOOTTT")
 	SignalHub.emit_on_create_bullet(
 		global_position, direction, speed, bullet_key
 	)
