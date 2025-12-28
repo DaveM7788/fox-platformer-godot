27e2a8e6a3f3ca7aeb19e4c9865fb4fc2e9cf1e0
DaveM7788 <david.mcdermott98@gmail.com>
2025-12-27 19:33:30 -0600

    adding boss
    boss uses animation tree and call method tracks

---
+++ Scenes/Boss/boss.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+@export var lives := 3
+@export var points := 5
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var hit_box: Area2D = $Visuals/HitBox
+@onready var shooter: Shooter = $Visuals/Shooter
+@onready var state_machine: AnimationNodeStateMachinePlayback = \
+	$AnimationTree["parameters/playback"]
+@onready var visuals: Node2D = $Visuals
+
+var _player_ref: Player
+var _invincible := false
+
+func _ready() -> void:
+	_player_ref = get_tree().get_first_node_in_group(Constants.PLAYER_GROUP)
+	if _player_ref == null:
+		queue_free()
+
+
+func shoot() -> void:
+	shooter.shoot(
+		shooter.global_position.direction_to(_player_ref.global_position)
+	)
+
+
+func activate_collisions() -> void:
+	hit_box.call_deferred("set_monitoring", true)
+	hit_box.call_deferred("set_monitorable", true)
+
+
+func _on_trigger_area_entered(area: Area2D) -> void:
+	animation_tree["parameters/conditions/on_trigger"] = true
+
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	take_damage()
+
+
+func take_damage() -> void:
+	if _invincible:
+		return
+	set_invincibile(true)
+	state_machine.travel("hit")
+	tween_hit()
+	reduce_lives()
+
+
+func tween_hit() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(visuals, "position", Vector2.ZERO, 1.8)
+
+
+func set_invincibile(on: bool) -> void:
+	_invincible = on
+
+
+func reduce_lives() -> void:
+	lives -= 1
+	if lives <= 0:
+		SignalHub.emit_on_scored(points)
+		queue_free()
